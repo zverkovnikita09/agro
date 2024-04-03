a8f7ce8d964005cd3a84d3ef08e533c25d94864e
Mikhail Lisovskii <f1ste@yandex.ru>
2024-04-04 02:24:57 +0300

    форма авторизации
    повторная отправка кода
    валидация

--- src/shared/ui/ErrorBlock/ErrorBlock.tsx
+++ src/shared/ui/ErrorBlock/ErrorBlock.tsx
@@ -1,11 +1,13 @@
 import style from './ErrorBlock.module.scss'
+import cn from "classnames";
 
 interface ErrorBlockProps {
   children: string
+  className?: string
 }
 
-export const ErrorBlock = ({ children }: ErrorBlockProps) => {
+export const ErrorBlock = ({ children, className }: ErrorBlockProps) => {
   return (
-    <p className={style.errorBlock}>{children}</p>
+    <p className={cn(style.errorBlock, className)}>{children}</p>
   )
 }
